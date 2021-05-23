07d5177b764107543ef5ece3b1a469c23d10b0d3
Ajmalva <84183186+Ajmalva@users.noreply.github.com>
2021-05-23 17:12:27 +0530

    Add files via upload
    Files for new UI of the web app

---
+++ static/js/control.js
@@ -0,0 +1,83 @@
+
+
+function updatetags (data) {
+  let original = `<img src="/${data.thumb_path}" class="responsive" alt="">`;
+  $("#original").html(original);
+
+  $("#howto").html("Drag and Drop file here<br />Or<br />Click to Upload")
+}
+
+//========================================================================
+// Drag and drop image handling
+//========================================================================
+
+var fileDrag = document.getElementById("file-drag");
+var fileSelect = document.getElementById("file-upload");
+
+// Add event listeners
+fileDrag.addEventListener("dragover", fileDragHover, false);
+fileDrag.addEventListener("dragleave", fileDragHover, false);
+fileDrag.addEventListener("drop", fileSelectHandler, false);
+fileSelect.addEventListener("change", fileSelectHandler, false);
+
+function fileDragHover(e) {
+  // prevent default behaviour
+  e.preventDefault();
+  e.stopPropagation();
+
+  fileDrag.className = e.type === "dragover" ? "upload-box dragover" : "upload-box";
+}
+
+function fileSelectHandler(e) {
+  // handle file selecting
+  var files = e.target.files || e.dataTransfer.files;
+  fileDragHover(e);
+  for (var i = 0, f; (f = files[i]); i++) {
+    previewFile(f);
+  }
+}
+
+//========================================================================
+// Web page elements for functions to use
+//========================================================================
+
+var imagePreview = document.getElementById("image-preview");
+var imageDisplay = document.getElementById("image-display");
+var uploadCaption = document.getElementById("upload-caption");
+var predResult = document.getElementById("pred-result");
+var loader = document.getElementById("loader");
+
+//========================================================================
+
+
+function previewFile(file) {
+  // show the preview of the image
+  console.log(file.name);
+  var fileName = encodeURI(file.name);
+
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    imagePreview.src = URL.createObjectURL(file);
+
+    show(imagePreview);
+    hide(uploadCaption);
+
+    // reset
+    predResult.innerHTML = "";
+    imageDisplay.classList.remove("loading");
+
+    displayImage(reader.result, "image-display");
+  };
+}
+
+
+function hide(el) {
+  // hide an element
+  el.classList.add("hidden");
+}
+
+function show(el) {
+  // show an element
+  el.classList.remove("hidden");
+}
